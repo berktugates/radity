8b4182abada7d29791dfbab50221b71f9a380b2a
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-05-25 03:40:22 +0300

    chore:update dependencies, prepare const values

--- frontend/constants/Colors.ts
+++ frontend/constants/Colors.ts
@@ -1,8 +1,10 @@
 export const Colors = {
+  SelectPlatformBg:"#F8F9FC",
   ByzantineBlue: "#1D4ED8",
   NeonAqua: "#5DFDCB",
   OxfordBlue:"#020833",
   PrimaryNavyOxford:"#676B85",
   Grayscale:"#F0F0F2",
+  Seasalt: "#F9F9F9",
   EstimateButon:"#1D4ED8"
 };
---
+++ frontend/constants/Questions.ts
@@ -0,0 +1,386 @@
+export const firstSection = [
+  {
+    id: "01",
+    title: "How big is your app?",
+    options: [
+      {
+        title: "Small",
+        description:
+          "Design based on a UI kit, utilizing pre-designed components",
+        icon: "/svg/question/smallDots.svg",
+      },
+      {
+        title: "Medium",
+        description: "Your app has around 4-7 key feature screens",
+        icon: "/svg/question/mediumDots.svg",
+      },
+      {
+        title: "Large",
+        description:
+          "Design based on a UI kit, utilizing pre-designed components",
+        icon: "/svg/question/largeDots.svg",
+      },
+    ],
+  },
+  {
+    id: "02",
+    title: "What level of UI would you like?",
+    options: [
+      {
+        title: "MVP",
+        description:
+          "Design based on a UI kit, utilizing pre-designed components",
+        icon: "/svg/question/mvp.svg",
+      },
+      {
+        title: "Basic",
+        description: "Partial use of UI Kit + customized design",
+        icon: "/svg/question/basic.svg",
+      },
+      {
+        title: "Polished",
+        description: "Fully customized design",
+        icon: "/svg/question/polished.svg",
+      },
+    ],
+  },
+  {
+    id: "03",
+    title: "Users & Accounts",
+    options: [
+      {
+        title: "Email/Password Sign up",
+        description: "Ability to log in using Email and password",
+        icon: "/svg/question/Mail.svg",
+      },
+      {
+        title: "Facebook Sign Up",
+        description: "Ability to log in using Facebook profile",
+        icon: "/svg/question/facebook.svg",
+      },
+      {
+        title: "Twitter Sign Up",
+        description: "Ability to log in using Twitter profile",
+        icon: "/svg/question/twitter.svg",
+      },
+      {
+        title: "Google Sign up",
+        description: "Ability to log in using Google account",
+        icon: "/svg/question/Google.svg",
+      },
+      {
+        title: "LinkedIn Sign up",
+        description: "Ability to log in using LinkedIn profile",
+        icon: "/svg/question/Linkedin.svg",
+      },
+      {
+        title: "Github Sign up",
+        description: "Ability to log in using Github account",
+        icon: "/svg/question/Github.svg",
+      },
+      {
+        title: "User Invitation Emails",
+        description: "Invite a user to try your app",
+        icon: "/svg/question/userInvitation.svg",
+      },
+      {
+        title: "Subdomains",
+        description: "An additional section of your main domain name",
+        icon: "/svg/question/Subdomains.svg",
+      },
+      {
+        title: "Custom Domains",
+        description: "A unique branded name that identifies a website",
+        icon: "/svg/question/customDomains.svg",
+      },
+    ],
+  },
+];
+
+export const secondSection = [
+  {
+    id: "04",
+    title: "Users Generated Content",
+    options: [
+      {
+        title: "Dashboard",
+        description:
+          "Your app's control center, where you can manage everything easily",
+        icon: "/svg/question/Dashboard.svg",
+      },
+      {
+        title: "Activity Feed",
+        description:
+          "A real-time stream showing updates and interactions within the app",
+        icon: "/svg/question/activityFeed.svg",
+      },
+      {
+        title: "Media Uploading",
+        description: "Tool to share media content effortlessly",
+        icon: "/svg/question/mediaUploading.svg",
+      },
+      {
+        title: "User Profiles",
+        description:
+          "A personalized space within the app for sharing information about oneself",
+        icon: "/svg/question/userProfiles.svg",
+      },
+      {
+        title: "Transactional Emails",
+        description: "Emails that keep users informed about important updates",
+        icon: "/svg/question/transactionalEmails.svg",
+      },
+      {
+        title: "Tags",
+        description: "Labels to help organize content and make it easy to find",
+        icon: "/svg/question/Tag.svg",
+      },
+      {
+        title: "Ratings or Reviews",
+        description: "Sharing opinions and experiences to empower customers",
+        icon: "/svg/question/Star.svg",
+      },
+      {
+        title: "Media Manipulation",
+        description: "Tools to edit and improve media files",
+        icon: "/svg/question/mediaManipulation.svg",
+      },
+      {
+        title: "Searching",
+        description: "System designed search through in the app",
+        icon: "/svg/question/Search.svg",
+      },
+    ],
+  },
+  {
+    id: "05",
+    title: "Dates & Locations",
+    options: [
+      {
+        title: "Calendaring",
+        description:
+          "Plan events and stay organized with an integrated calendar feature",
+        icon: "/svg/question/Calendaring.svg",
+      },
+      {
+        title: "Display of Map Data",
+        description: "Display locations seamlessly through interactive maps",
+        icon: "/svg/question/Map.svg",
+      },
+      {
+        title: "Geolocation",
+        description:
+          "Pinpoint exact location or find nearby points of interest effortlessly",
+        icon: "/svg/question/Geolocation.svg",
+      },
+      {
+        title: "Bookings",
+        description: "Easily book services with streamlined functionality",
+        icon: "/svg/question/Booking.svg",
+      },
+    ],
+  },
+  {
+    id: "06",
+    title: "Social & Engagement",
+    options: [
+      {
+        title: "Messaging",
+        description: "Communicate with app users through messaging",
+        icon: "/svg/question/Messaging.svg",
+      },
+      {
+        title: "Forums or Commenting",
+        description: "Engage in discussions and leave comments on forums",
+        icon: "/svg/question/forums.svg",
+      },
+      {
+        title: "Social Sharing",
+        description:
+          "Share content easily across various social media platforms",
+        icon: "/svg/question/socialSharing.svg",
+      },
+      {
+        title: "Push to Facebook Open Graph",
+        description: "Integrate and share app activities on Facebook",
+        icon: "/svg/question/pushToFace.svg",
+      },
+      {
+        title: "Push Notifications",
+        description:
+          "Stay informed with timely updates and notifications pushed to device",
+        icon: "/svg/question/pushNotifications.svg",
+      },
+    ],
+  },
+  {
+    id: "07",
+    title: "Billing & eCommerce",
+    options: [
+      {
+        title: "Subscription Plans",
+        description:
+          "Charge your subscribers, setup automatic renewals and trials",
+        icon: "/svg/question/subscriptionPlans.svg",
+      },
+      {
+        title: "Shopping Cart",
+        description:
+          "Enables you to select and review items, and complete purchases",
+        icon: "/svg/question/shoppingCart.svg",
+      },
+      {
+        title: "User Marketplace",
+        description:
+          "Allows users to buy and sell a variety of items within your app",
+        icon: "/svg/question/Marketplace.svg",
+      },
+      {
+        title: "In-App Purchasing",
+        description:
+          "Users can effortlessly make purchases directly from your app",
+        icon: "/svg/question/inApp.svg",
+      },
+      {
+        title: "Payment Information Collection",
+        description:
+          "Securely store and manage user payment details for transactions",
+        icon: "/svg/question/paymentInfo.svg",
+      },
+      {
+        title: "Payment Processing",
+        description:
+          "Ensure secure and efficient handling of transactions within your app",
+        icon: "/svg/question/paymentProcess.svg",
+      },
+      {
+        title: "Product Management",
+        description:
+          "Set the strategy, roadmap, and feature definition for products",
+        icon: "/svg/question/productManagement.svg",
+      },
+    ],
+  },
+];
+
+export const thirdSection = [
+  {
+    id: "08",
+    title: "Admin, Feedback, Analytics",
+    options: [
+      {
+        title: "CMS Integration",
+        description: "Seamlessly manage and update content",
+        icon: "/svg/question/CMS.svg",
+      },
+      {
+        title: "User Admin Pages",
+        description: "Gain control over user management and permissions",
+        icon: "/svg/question/userAdminPages.svg",
+      },
+      {
+        title: "Moderation/Content Approval",
+        description: "Allowing you to moderate and control the content shared",
+        icon: "/svg/question/moderation.svg",
+      },
+      {
+        title: "Support",
+        description: "Provide assistance and address user queries",
+        icon: "/svg/question/Support.svg",
+      },
+      {
+        title: "Usage Analytics",
+        description:
+          "Gain valuable insights into user behavior and interaction",
+        icon: "/svg/question/usageAnalytics.svg",
+      },
+      {
+        title: "Crash Reporting",
+        description:
+          "Receive real-time alerts and insights into app performance issues",
+        icon: "/svg/question/crashReport.svg",
+      },
+      {
+        title: "Performance Monitoring",
+        description: "Track and enhance your app's overall efficiency",
+        icon: "/svg/question/performanceMonitoring.svg",
+      },
+      {
+        title: "Multilingual Support",
+        description:
+          "Cater to a diverse audience by offering your app in multiple languages",
+        icon: "/svg/question/multilingual.svg",
+      },
+    ],
+  },
+  {
+    id: "09",
+    title: "External APIs and Integrations Hours",
+    options: [
+      {
+        title: "Connect to one or more third-party services",
+        description:
+          "Link your app with external services, expanding functionality and features",
+        icon: "/svg/question/Connect.svg",
+      },
+      {
+        title: "An API for others to integrate with your app",
+        description:
+          "Enable external parties to integrate their services with your app",
+        icon: "/svg/question/API.svg",
+      },
+      {
+        title: "SMS Messaging",
+        description: "For direct and instant communication within your app",
+        icon: "/svg/question/SMS.svg",
+      },
+      {
+        title: "Phone Number Masking",
+        description:
+          "Enhancing privacy and security for users when exchanging contact info",
+        icon: "/svg/question/phoneMask.svg",
+      },
+    ],
+  },
+  {
+    id: "10",
+    title: "Security Hours",
+    options: [
+      {
+        title: "SSL Certificate based Security",
+        description:
+          "Ensure encrypted and secure communication between users and your app",
+        icon: "/svg/question/SSL.svg",
+      },
+      {
+        title: "DDoS Protection",
+        description:
+          "For safeguarding your app against potential Distributed Denial of Service attacks",
+        icon: "/svg/question/DDoS.svg",
+      },
+      {
+        title: "Two Factor Authentication",
+        description:
+          "Requiring an additional layer of verification during login enhance security",
+        icon: "/svg/question/twoFactor.svg",
+      },
+    ],
+  },
+  {
+    id: "11",
+    title: "App Specific Development Hours",
+    options: [
+      {
+        title: "App Specific Development",
+        description: "Customized app that meets unique needs",
+        icon: "/svg/question/appSpecific.svg",
+      },
+      {
+        title: "Project Manager",
+        description:
+          "Befit from coordination and oversight for your app's specific development task",
+        icon: "/svg/question/projectManager.svg",
+      },
+    ],
+  },
+];
