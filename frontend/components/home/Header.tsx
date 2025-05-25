cfa1e27c420a6b72edf2406a0c38d2fdbacc7a4f
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-05-25 03:44:29 +0300

    fix: update header component

--- frontend/components/home/Header.tsx
+++ frontend/components/home/Header.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className="lg:w-full lg:flex lg:items-center lg:pt-14 lg:pb-24 lg:px-16">
+    <div className="bg-gradient-to-r from-[#1D4ED8] via-[#2A72D5] to-[#5DFDCB]  lg:w-full lg:flex lg:items-center lg:pt-14 lg:pb-24 lg:px-16">
       <div id="header-texts" className="lg:flex lg:flex-col lg:gap-y-8">
-        <h1 className="lg:text-5xl lg:leading-[57.6px] lg:font-extrabold">
+        <h1 className="text-white lg:text-5xl lg:leading-[57.6px] lg:font-extrabold">
           Navigate Your App&apos;s Development Journey
         </h1>
-        <p className="lg:text-xl lg:leading-[1.4]">
+        <p className="text-white lg:text-xl lg:leading-[1.4]">
           Our expertise-backed Project Estimation Tool assists in calculating
           projected development time and budget. Choose options below that best
           characterize your app, outlining the features you need for a precise
@@ -16,7 +16,7 @@ export const Header = () => {
         </p>
       </div>
       <Image
-        src={"/img/Header.png"}
+        src={"/img/banner.png"}
         width={1440}
         height={750}
         alt="header"
