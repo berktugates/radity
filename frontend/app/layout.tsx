b88d3ff31acdbc56b225be3a69489afc0fd30f33
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-05-25 21:07:14 +0300

    chore:removed unused fonts

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Mulish } from "next/font/google";
+import { Mulish } from "next/font/google";
 import "./globals.css";
 import { Container } from "./Container";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const mulish = Mulish({
   variable:"--font-mulish",
